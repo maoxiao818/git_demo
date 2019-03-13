41c6be74e9cde1ad1de9c27290daaaaaa5b8f77f
maoxiao818 <525382763@qq.com>
2019-03-13 14:29:39 +0800

    2019/03/13 14:29

--- git_demo/src/git_demo/GitTest1.java
+++ git_demo/src/git_demo/GitTest1.java
@@ -4,11 +4,6 @@ public class GitTest1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		System.out.println("hello world2!");
-		System.out.println("hello world3!");
-		System.out.println("hello world3!");
-		System.out.println("hello world6!");
-		System.out.println("hello world3!");
 		System.out.println("hello world3!");
 	}
 }
